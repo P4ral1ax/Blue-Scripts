dcf92c310c7481b844fb31da1f257e1165f21ea8
PapaChain <braydenjw00@gmail.com>
2021-10-09 14:41:29 -0400

    Working go_finder

--- go_finder.sh
+++ go_finder.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+
 # Find Pesky GO files. The red teamers seem to love it. 
+# Find any large binary files
 
+# sudo find /  -type f -executable -size +1M -exec strings {} + | grep 'go1\.'
 
+# Make Array
+files=()
 
-out = sudo find / -perm 755 -type f -size +1M
+# Put all binaries larger than 1Mb into array
+## TAKE NOTE OF EXCLUSION
+mapfile -d $'\0' files < <(sudo find / -type f -executable -size +1M  \( ! -path '*snap*' \) -print0 2>/dev/null)
 
-FILES=', ' read -r -a array <<< "$string"
+# Check each file
+for i in "${files[@]}"
+do
+    if [[ $(strings $i 2>/dev/null | grep 'go1\.' 2>/dev/null) ]]; then   
+        echo "Detected GO Binary : $i"
+    else
+        continue
+    fi
+done
